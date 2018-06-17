855df0e8d832b73d4729e85323c8ee52a971a49a
Monika Zmudziak <monika.zmudziak@gmail.com>
2018-06-17 17:13:55 +0200

    Konflikt

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,8 +2,11 @@ package com.company;
 
 public class Main {
 
+    private int test;
+
     public static void main(String[] args) {
         System.out.println("MavenTest");
         System.out.println("Pierwsza zmiana");
+        System.out.println("Zmiana na masterze");
     }
 }
